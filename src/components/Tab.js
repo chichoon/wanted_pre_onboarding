737da5892bd40e7ca525a90e1f3807f5d29053a5
chichoon <chichoon.choi@gmail.com>
2022-04-15 20:06:51 +0900

    Design: Tab 레이아웃 조정, 선택항목에 따른 다른 style 적용

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -15,19 +15,75 @@ const Tab = ({ selectorArr }) => {
       <div className="selector-parent" selectedIndex={selectedIndex}>
         <div className="front">
           {selectorArr.map((v, i) => (
-            <div
+            <SelectorDiv
               className={selectedIndex === i ? 'selected' : ''}
               key={i}
               onClick={e => handleOnClick(e, i)}
+              length={selectorArr.length}
             >
               {selectorArr[i]}
-            </div>
+            </SelectorDiv>
           ))}
         </div>
-        <div className="back"></div>
+        <div className="back">
+          <div></div>
+        </div>
       </div>
     </TabWrapper>
   );
 };
+
+const TabWrapper = Styled.div`
+  width: 30rem;
+  height: 4rem;
+
+  padding: 0.3rem;
+  background-color: white;
+
+  display: flex;
+  justify-content: center;
+
+  .selector-parent {
+    width: 25rem;
+    position: relative;
+    z-index: 3;
+
+    & > div {
+      width: 25rem;
+      height: 4rem;
+
+      position: absolute;
+      left: 0px;
+      top: 0px;
+    }
+
+    .front {
+      display: flex;
+      flex-direction: row;
+      z-index: 2;
+    }
+
+    .back {
+      z-index: 1;
+      background-color: lightgray;
+    }
+  }
+`;
+
+const SelectorDiv = Styled.div`
+  width: calc(100% / ${props => props.length});
+
+  margin-bottom: 0.3rem;
+  background-color: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: font-weight 0.2s;
+  &.selected {
+    font-weight: 700;
+  }
+`;
 
 export default Tab;
