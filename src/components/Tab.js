5aabea5070e58003a3d3249b4ea1b2cce1f47864
chichoon <chichoon.choi@gmail.com>
2022-04-15 19:48:04 +0900

    Feat: Tab div pseudo 레이아웃 조정

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Tab = () => {
-  return <div>tab</div>;
+import Styled from 'styled-components';
+
+const Tab = ({ selectorArr }) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleOnClick = (e, index) => {
+    e.preventDefault();
+    setSelectedIndex(index);
+  };
+
+  return (
+    <TabWrapper>
+      <div className="selector-parent" selectedIndex={selectedIndex}>
+        <div className="front">
+          {selectorArr.map((v, i) => (
+            <div
+              className={selectedIndex === i ? 'selected' : ''}
+              key={i}
+              onClick={e => handleOnClick(e, i)}
+            >
+              {selectorArr[i]}
+            </div>
+          ))}
+        </div>
+        <div className="back"></div>
+      </div>
+    </TabWrapper>
+  );
 };
 
 export default Tab;
