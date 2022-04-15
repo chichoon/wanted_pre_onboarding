891a5cc61f587af8c7ae5c8ae0db70d074fe5c36
chichoon <chichoon.choi@gmail.com>
2022-04-15 18:30:09 +0900

    Feat: App.js에서 컴포넌트 변화값 볼 수 있도록 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import { React, useState } from 'react';
+
+import Styled from 'styled-components';
 import { Dropdown, Input, Slider, Tab, Toggle } from './components';
 
+import './App.css';
+
 function App() {
+  const [toggleSelected, setToggleSelected] = useState('');
+
   return (
     <div className="App">
-      <Toggle firstString="기본" secondString="상세" />
+      <ComponentWrapper>
+        <Toggle
+          firstString="기본"
+          secondString="상세"
+          setFunc={setToggleSelected}
+        />
+        <div>Selected: {toggleSelected}</div>
+      </ComponentWrapper>
       <Tab />
       <Slider />
       <Input />
@@ -12,5 +25,14 @@ function App() {
     </div>
   );
 }
+
+const ComponentWrapper = Styled.div`
+  margin: 0.5rem;
+  margin-bottom: 1rem;
+
+  & > div {
+    margin: 0.5rem;
+  }
+`;
 
 export default App;
