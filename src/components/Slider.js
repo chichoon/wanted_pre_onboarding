43c7b147a8f9efe8050b268aa4e92230bf977068
chichoon <chichoon.choi@gmail.com>
2022-04-17 14:02:16 +0900

    Design: 위의 값 표시 부분 스타일 작업

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,7 @@
 import { React, useEffect, useState } from 'react';
 
+import Styled from 'styled-components';
+
 const Slider = ({ setFunc }) => {
   const [sliderValue, setSliderValue] = useState(0);
 
@@ -7,7 +9,81 @@ const Slider = ({ setFunc }) => {
     setFunc(sliderValue);
   }, [sliderValue]);
 
-  return <div>slider</div>;
+  return (
+    <SliderWrapper>
+      <div className="slider-head">
+        <div>{sliderValue}</div>
+        <div>%</div>
+      </div>
+      <SliderBody setSliderFunc={setSliderValue} />
+    </SliderWrapper>
+  );
 };
+
+const SliderBody = ({ setSliderFunc }) => {
+  /*
+  const handleOnDrag = e => {
+    console.log(e);
+    setSliderValue(sliderValue + 1);
+  };
+*/
+  setSliderFunc(0);
+  return (
+    <SliderBodyWrapper>
+      <div className="front"></div>
+      <div className="back">
+        <div className="background">
+          <div></div>
+          <div className="stick"></div>
+        </div>
+      </div>
+    </SliderBodyWrapper>
+  );
+};
+
+const SliderWrapper = Styled.div`
+  width: 20rem;
+  height: 8rem;
+
+  padding: 0.3rem;
+  border: 1px solid blue;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .slider-head {
+    width: 18rem;
+    height: 3rem;
+    background-color: #EEE;
+
+    border: 1px solid #AAA;
+    border-radius: 0.3rem;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: right;
+    align-items: center;
+
+    div:nth-child(1) {
+      width: 12rem;
+
+      padding: 0.5rem 0.6rem;
+      text-align: right;
+      font-weight: 700;
+    }
+    div:nth-child(2) {
+      margin: 0 0.5rem;
+      color: rgba(0, 0, 0, 0.5);
+    }
+  }
+`;
+
+const SliderBodyWrapper = Styled.div`
+  width: 18rem;
+  height: 3rem;
+  border: 1px solid black;
+`;
 
 export default Slider;
