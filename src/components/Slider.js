12124ffbfa24755e750c0c0492baca755260dd4d
chichoon <chichoon.choi@gmail.com>
2022-04-17 13:38:38 +0900

    Feat: Slider 기본사항 (인자 받아오기 등) 세팅

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 
-const Slider = () => {
+const Slider = ({ setFunc }) => {
+  const [sliderValue, setSliderValue] = useState(0);
+
+  useEffect(() => {
+    setFunc(sliderValue);
+  }, [sliderValue]);
+
   return <div>slider</div>;
 };
 
