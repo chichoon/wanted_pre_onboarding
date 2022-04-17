47a0b6e58bec735097cb200b88e70bd9c3d05754
chichoon <chichoon.choi@gmail.com>
2022-04-17 15:11:23 +0900

    Design: 슬라이더 디자인 수정중

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,6 +4,8 @@ import Styled from 'styled-components';
 
 const Slider = ({ setFunc }) => {
   const [sliderValue, setSliderValue] = useState(0);
+  //const [dragStart, setDragStart] = useState(0);
+  //const [dragEnd, setDragEnd] = useState(0);
 
   useEffect(() => {
     setFunc(sliderValue);
@@ -21,16 +23,16 @@ const Slider = ({ setFunc }) => {
 };
 
 const SliderBody = ({ setSliderFunc }) => {
-  /*
-  const handleOnDrag = e => {
-    console.log(e);
-    setSliderValue(sliderValue + 1);
+  const handleOnChange = e => {
+    console.log(e.target.value);
+    setSliderFunc(1);
   };
-*/
-  setSliderFunc(0);
+
   return (
     <SliderBodyWrapper>
-      <div className="front"></div>
+      <div className="front">
+        <SliderStyledInput type="range" onChange={handleOnChange} />
+      </div>
       <div className="back">
         <div className="background">
           <div></div>
@@ -43,7 +45,7 @@ const SliderBody = ({ setSliderFunc }) => {
 
 const SliderWrapper = Styled.div`
   width: 20rem;
-  height: 8rem;
+  height: 10rem;
 
   padding: 0.3rem;
   border: 1px solid blue;
@@ -81,9 +83,64 @@ const SliderWrapper = Styled.div`
 `;
 
 const SliderBodyWrapper = Styled.div`
-  width: 18rem;
-  height: 3rem;
+  width: 17.2rem;
+  height: 4.2rem;
+  padding: 0.4rem;
+  margin-top: 0.5rem;
   border: 1px solid black;
+
+  display: flex;
+  justify-content: center;
+
+  position: relative;
+
+  .front {
+    width: inherit;
+    height: 2rem;
+    padding: 0.1rem;
+    border: 1px solid red;
+
+    display: flex;
+    align-items: center;
+`;
+
+const SliderStyledInput = Styled.input.attrs({ type: 'range' })`
+-webkit-appearance: none;
+width: 100%;
+height: 1rem;
+background-color: transparent;
+
+&:focus {
+  outline: none;
+}
+
+&::-webkit-slider-runnable-track {
+  -webkit-appearance: none;
+  height: 0.3rem;
+  border-radius: 0.1rem;
+  background-color: #EEE;
+}
+
+&::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  width: 1rem;
+  height: 1rem;
+  margin: 0px;
+  border: 3px solid white;
+  border-radius: 50%;
+  background: teal;
+}
+
+&::-moz-range-track {
+  height: 2rem;
+  border-radius: 0.1rem;
+  background-color: #EEE;
+}
+
+&::-moz-range-thumb {
+  background-color: yellow;
+}
+
 `;
 
 export default Slider;
