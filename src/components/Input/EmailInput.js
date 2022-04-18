0037d2fbfd0a1f279bd7964b1632645f283f1b43
chichoon <chichoon.choi@gmail.com>
2022-04-18 19:44:28 +0900

    Feat: Input 컴포넌트 두개로 분리, 스타일 적용

---
+++ src/components/Input/EmailInput.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const EmailInput = ({ className, inputEmail, setInputEmail }) => {
+  const [ifEmailValid, setIfEmailValid] = useState(false);
+  const [ifFocus, setIfFocus] = useState(false);
+
+  const handleOnEmailChange = e => {
+    const regex = /[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/;
+    setInputEmail(e.target.value);
+    if (e.target.value.match(regex)) setIfEmailValid(true);
+    else setIfEmailValid(false);
+  };
+
+  const handleOnFocus = e => {
+    e.preventDefault();
+    setIfFocus(ifFocus ? false : true);
+  };
+
+  return (
+    <div className={className}>
+      <label htmlFor="email">E-mail</label>
+      <div>
+        <input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          value={inputEmail}
+          onChange={handleOnEmailChange}
+          onFocus={handleOnFocus}
+        />
+        <label hidden={ifFocus ? true : false}>Invalid E-mail address.</label>
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ fill: `${ifEmailValid ? 'teal' : '#CCC'}` }}
+        >
+          <path
+            className="cls-1"
+            d="M16,31A15,15,0,1,1,31,16,15,15,0,0,1,16,31ZM16,3A13,13,0,1,0,29,16,13,13,0,0,0,16,3Z"
+          />
+          <path
+            className="cls-1"
+            d="M13.67,22a1,1,0,0,1-.73-.32l-4.67-5a1,1,0,0,1,1.46-1.36l3.94,4.21,8.6-9.21a1,1,0,1,1,1.46,1.36l-9.33,10A1,1,0,0,1,13.67,22Z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default EmailInput;
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,41 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import Styled from 'styled-components';
-
-const CheckIcon = () => {
-  return (
-    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-      <path
-        className="cls-1"
-        d="M16,31A15,15,0,1,1,31,16,15,15,0,0,1,16,31ZM16,3A13,13,0,1,0,29,16,13,13,0,0,0,16,3Z"
-      />
-      <path
-        className="cls-1"
-        d="M13.67,22a1,1,0,0,1-.73-.32l-4.67-5a1,1,0,0,1,1.46-1.36l3.94,4.21,8.6-9.21a1,1,0,1,1,1.46,1.36l-9.33,10A1,1,0,0,1,13.67,22Z"
-      />
-    </svg>
-  );
-};
-
-const EyeIcon = () => {
-  return (
-    <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
-      <path d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z" />
-    </svg>
-  );
-};
+import EmailInput from './EmailInput';
+import PasswordInput from './PasswordInput';
 
 const Input = ({ setFuncEmail, setFuncPassword }) => {
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
-
-  const handleOnEmailChange = e => {
-    setInputEmail(e.target.value);
-  };
-
-  const handleOnPasswordChange = e => {
-    setInputPassword(e.target.value);
-  };
 
   useEffect(() => {
     setFuncEmail(inputEmail);
@@ -44,62 +15,58 @@ const Input = ({ setFuncEmail, setFuncPassword }) => {
 
   return (
     <InputWrapper>
-      <label htmlFor="email">E-mail</label>
-      <input
-        type="email"
-        name="email"
-        placeholder="E-mail"
-        value={inputEmail}
-        onChange={handleOnEmailChange}
-        pattern="/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+      <EmailInput
+        className="input-div"
+        inputEmail={inputEmail}
+        setInputEmail={setInputEmail}
       />
-      <CheckIcon />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={inputPassword}
-        onChange={handleOnPasswordChange}
+      <PasswordInput
+        className="input-div"
+        inputPassword={inputPassword}
+        setInputPassword={setInputPassword}
       />
-      <EyeIcon />
     </InputWrapper>
   );
 };
 
 const InputWrapper = Styled.div`
   width: 15rem;
-  height: rem;
-  padding: 0.5rem;
-
-  border: 1px solid blue;
+  height: 7rem;
+  padding: 1rem;
 
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  label {
-    width: 14rem;
-    padding: 0.3rem;
+  .input-div {
+    & > div {
+      position: relative;
+    }
 
-    text-align: left;
-    font-size: 0.5rem;
-    color: rgba(0, 0, 0, 0.5);
-  }
+    label {
+      width: 14rem;
+      padding: 0.3rem 0.5rem;
 
-  input {
-    width: 13rem;
-    height: 2rem;
-    padding: 0 0.5rem;
-    background-color: #EEE;
+      text-align: left;
+      font-size: 0.5rem;
+      color: rgba(0, 0, 0, 0.5);
 
-    border: 1px solid #AAA;
-    border-radius: 0.2rem;
-  }
+      display: flex;
+      flex-direction: column;
+      justify-content: left;
+    }
 
-  input:nth-child(2) {
-    margin-bottom: 1rem;
+    input {
+      width: 13rem;
+      height: 2rem;
+      padding: 0 0.5rem;
+      padding-right: 2rem;
+      background-color: #EEE;
+
+      border: 1px solid #AAA;
+      border-radius: 0.2rem;
+    }
   }
 
   input:focus {
@@ -111,6 +78,12 @@ const InputWrapper = Styled.div`
     width: 1rem;
     height: 1rem;
     color: black;
+
+    position: absolute;
+    right: 0.5rem;
+    top: 0.5rem;
+
+    transition: fill 0.2s;
   }
 `;
 
