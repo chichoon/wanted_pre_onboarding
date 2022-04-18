8d34a58ff4a54cc44a15a860b901340203dfe272
chichoon <chichoon.choi@gmail.com>
2022-04-18 14:45:52 +0900

    Feat: Input 기본 틀 완성

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,78 @@
 import React, { useEffect, useState } from 'react';
 
+import Styled from 'styled-components';
+
 const Input = ({ setFuncEmail, setFuncPassword }) => {
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
 
+  const handleOnEmailChange = e => {
+    setInputEmail(e.target.value);
+  };
+
+  const handleOnPasswordChange = e => {
+    setInputPassword(e.target.value);
+  };
+
   useEffect(() => {
     setFuncEmail(inputEmail);
     setFuncPassword(inputPassword);
-  });
-  return <div>input</div>;
+  }, [inputEmail, inputPassword]);
+
+  return (
+    <InputWrapper>
+      <label htmlFor="email">E-mail</label>
+      <input
+        type="email"
+        name="email"
+        placeholder="E-mail"
+        value={inputEmail}
+        onChange={handleOnEmailChange}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={inputPassword}
+        onChange={handleOnPasswordChange}
+      />
+    </InputWrapper>
+  );
 };
+
+const InputWrapper = Styled.div`
+  width: 15rem;
+  height: rem;
+  padding: 0.5rem;
+
+  border: 1px solid blue;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  label {
+    width: 14rem;
+    padding: 0.3rem;
+
+    text-align: left;
+    font-size: 0.5rem;
+    color: rgba(0, 0, 0, 0.5);
+  }
+
+  input {
+    width: 13rem;
+    height: 2rem;
+    padding: 0 0.5rem;
+
+    border: none;
+    border: 1px solid black;
+  }
+  input:nth-child(2) {
+    margin-bottom: 1rem;
+  }
+`;
 
 export default Input;
