21c91b27b31069a7567eb07be1055b1d92d5d3f8
chichoon <chichoon.choi@gmail.com>
2022-05-06 20:52:46 +0900

    Feat: DropdownBottom 합침, div -> ul, li로 수정

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,34 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+import { useClickAway } from 'react-use';
+import cx from 'classnames';
 
 import styles from './Dropdown.module.scss';
-import { TopArrow } from '../../svgs';
-import DropdownBottom from './DropdownBottom';
+import { SearchIcon, TopArrow } from '../../assets/svgs';
 
 const Dropdown = ({ dropdownArr, setFunc }) => {
   const [selectedStr, setSelectedStr] = useState(dropdownArr[0]);
-  const [ifHidden, setIfHidden] = useState(true);
+  const [isHidden, setIsHidden] = useState(true);
+  const [searchInput, setSearchInput] = useState('');
+  const ref = useRef(null);
 
-  const handleOnClick = e => {
+  const handleOnChange = e => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleTopClick = e => {
     e.preventDefault();
-    setIfHidden(ifHidden ? false : true);
+    setIsHidden(isHidden ? false : true);
+  };
+
+  const handleListClick = (e, v) => {
+    e.preventDefault();
+    setSelectedStr(v);
+    setIsHidden(true);
   };
 
   useEffect(() => {
     setFunc(selectedStr);
   }, [selectedStr]);
 
+  useClickAway(ref, () => {
+    console.log('clicked away');
+    setIsHidden(true);
+  });
+
   return (
     <div className={styles.dropdownDiv}>
-      <div className={styles.dropdownTop} onClick={handleOnClick}>
+      <div className={styles.dropdownTop} onClick={handleTopClick}>
         <div className={styles.dropdownTopHeader}>{selectedStr}</div>
         <TopArrow className={styles.topArrow} />
       </div>
-      <DropdownBottom
-        dropdownArr={dropdownArr}
-        ifHidden={ifHidden}
-        setIfHidden={setIfHidden}
-        setStr={setSelectedStr}
-      />
+      <div className={styles.dropdownBottom} ref={ref}>
+        <div
+          className={cx(styles.searchInput, { [styles.isHidden]: isHidden })}
+        >
+          <SearchIcon className={styles.searchIcon} />
+          <input
+            type="text"
+            onChange={handleOnChange}
+            value={searchInput}
+            placeholder="Search Symbol"
+          />
+        </div>
+        <ul className={styles.selectList}>
+          {dropdownArr
+            .filter(v => {
+              if (searchInput === '') return v;
+              else if (v.toLowerCase().includes(searchInput)) return v;
+            })
+            .map((v, i) => (
+              <li key={i} onClick={e => handleListClick(e, v)}>
+                {v}
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
