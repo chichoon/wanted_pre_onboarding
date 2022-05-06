e7d1de2cd07118aa9ca88aadd27e26023b960635
chichoon <chichoon.choi@gmail.com>
2022-05-06 16:53:10 +0900

    Design: Dropdown Styled 대신 CSS Module로 변경

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-import Styled from 'styled-components';
+import styles from './Dropdown.module.scss';
+import { TopArrow } from '../../svgs';
 import DropdownBottom from './DropdownBottom';
 
 const Dropdown = ({ dropdownArr, setFunc }) => {
@@ -17,20 +18,10 @@ const Dropdown = ({ dropdownArr, setFunc }) => {
   }, [selectedStr]);
 
   return (
-    <DropdownWrapper>
-      <div className="dropdown-top" onClick={handleOnClick}>
-        <div className="dropdown-top-header">{selectedStr}</div>
-        <div className="dropdown-top-arrow">
-          <svg
-            height="32"
-            id="triangle-down"
-            viewBox="0 0 32 32"
-            width="32"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M4 8 H28 L16 26 z" />
-          </svg>
-        </div>
+    <div className={styles.dropdownDiv}>
+      <div className={styles.dropdownTop} onClick={handleOnClick}>
+        <div className={styles.dropdownTopHeader}>{selectedStr}</div>
+        <TopArrow className={styles.topArrow} />
       </div>
       <DropdownBottom
         dropdownArr={dropdownArr}
@@ -38,55 +29,8 @@ const Dropdown = ({ dropdownArr, setFunc }) => {
         setIfHidden={setIfHidden}
         setStr={setSelectedStr}
       />
-    </DropdownWrapper>
+    </div>
   );
 };
-
-const DropdownWrapper = Styled.div`
-  width: 20rem;
-  height: fit-content;
-
-  display: flex;
-  flex-direction : column;
-  align-items: center;
-
-  .dropdown-top {
-    width: 12rem;
-    height: 2rem;
-    padding: 0.2rem 0.3rem;
-    background-color: #FAFAFA;
-
-    border: 1px solid #AAA;
-    border-radius: 0.3rem;
-
-    display: flex;
-    flex-direction : row;
-    align-items: center;
-    justify-content: space-between;
-
-    .dropdown-top-header {
-      width: 8rem;
-      padding: 0 0.2rem;
-
-      font-size: 0.8rem;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      white-space: nowrap;
-    }
-
-    .dropdown-top-arrow {
-      width: 2rem;
-      height: inherit;
-      svg {
-        width: 1rem;
-        fill: #666;
-      }
-    }
-
-    &:hover {
-      cursor: pointer;
-    }
-  }
-`;
 
 export default Dropdown;
