082e434139ca745d58bae41d4761ddcd98020296
chichoon <chichoon.choi@gmail.com>
2022-05-07 20:23:24 +0900

    Feat: 원 아이콘 svg 추가

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,4 +1,5 @@
-export { ReactComponent as SearchIcon } from './SearchIcon.svg';
-export { ReactComponent as TopArrowIcon } from './TopArrowIcon.svg';
-export { ReactComponent as CheckIcon } from './CheckIcon.svg';
-export { ReactComponent as EyeIcon } from './EyeIcon.svg';
+export { ReactComponent as SearchIcon } from './SearchIcon.svg'
+export { ReactComponent as TopArrowIcon } from './TopArrowIcon.svg'
+export { ReactComponent as CheckIcon } from './CheckIcon.svg'
+export { ReactComponent as EyeIcon } from './EyeIcon.svg'
+export { ReactComponent as CircleIcon } from './CircleIcon.svg'
