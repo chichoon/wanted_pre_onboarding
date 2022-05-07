2859967534ab2927056d2a754a935e37a7cc5410
chichoon <chichoon.choi@gmail.com>
2022-05-07 13:39:43 +0900

    Feat: eslintrc, prettierrc 재설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  semi: false,
+  printWidth: 120,
+  proseWrap: never,
+  singleQuote: true,
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+};
