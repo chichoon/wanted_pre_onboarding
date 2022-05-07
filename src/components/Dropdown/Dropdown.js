a3381c1963feeb4dff3fc8bfcd963de687022063
chichoon <chichoon.choi@gmail.com>
2022-05-07 13:52:15 +0900

    Feat: Dropdown.js 린터 재설정

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react'
 
-import { useClickAway } from 'react-use';
-import cx from 'classnames';
+import { useClickAway } from 'react-use'
+import cx from 'classnames'
 
-import styles from './Dropdown.module.scss';
-import { SearchIcon, TopArrowIcon } from '../../assets/svgs';
+import styles from './Dropdown.module.scss'
+import { SearchIcon, TopArrowIcon } from '../../assets/svgs'
 
-const Dropdown = ({ dropdownArr, setFunc }) => {
-  const [selectedStr, setSelectedStr] = useState(dropdownArr[0]);
-  const [isHidden, setIsHidden] = useState(true);
-  const [searchInput, setSearchInput] = useState('');
-  const ref = useRef(null);
+function Dropdown ({ dropdownArr, setFunc }) {
+  const [selectedStr, setSelectedStr] = useState(dropdownArr[0])
+  const [isHidden, setIsHidden] = useState(true)
+  const [searchInput, setSearchInput] = useState('')
+  const ref = useRef(null)
 
-  const handleOnChange = e => setSearchInput(e.target.value);
+  const handleOnChange = e => setSearchInput(e.target.value)
 
-  const handleTopClick = () => setIsHidden(false);
+  const handleTopClick = () => setIsHidden(false)
 
   const handleListClick = v => {
-    setSelectedStr(v);
-    setIsHidden(true);
-  };
+    setSelectedStr(v)
+    setIsHidden(true)
+  }
 
   useEffect(() => {
-    setFunc(selectedStr);
-  }, [selectedStr]);
+    setFunc(selectedStr)
+  }, [setFunc, selectedStr])
 
-  useClickAway(ref, () => setIsHidden(true));
+  useClickAway(ref, () => setIsHidden(true))
 
   return (
     <div className={styles.dropdownDiv}>
-      <button className={styles.dropdownTop} onClick={handleTopClick}>
+      <button type="button" className={styles.dropdownTop} onClick={handleTopClick}>
         <span className={styles.dropdownTopSpan}>{selectedStr}</span>
         <TopArrowIcon className={styles.svgs} />
       </button>
@@ -49,18 +49,21 @@ const Dropdown = ({ dropdownArr, setFunc }) => {
         <ul className={styles.selectList}>
           {dropdownArr
             .filter(v => {
-              if (searchInput === '') return v;
-              else if (v.toLowerCase().includes(searchInput)) return v;
+              if (searchInput === '') return v
+              if (v.toLowerCase().includes(searchInput)) return v
+              return 0
             })
-            .map((v, i) => (
-              <li key={`dropdown-list-${i}`} onClick={() => handleListClick(v)}>
-                {v}
+            .map((v) => (
+              <li key={`dropdown-list-${v}`}>
+                <button type="button" onClick={() => handleListClick(v)}>
+                  {v}
+                </button>
               </li>
             ))}
         </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Dropdown;
+export default Dropdown
