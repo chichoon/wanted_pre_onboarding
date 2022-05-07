1fa0bb82b23eee2a24cfddbe5fdda82823619787
chichoon <chichoon.choi@gmail.com>
2022-05-07 17:08:03 +0900

    Design: Input 클래스명 classnames와 모듈로 재설정

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,61 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import cx from 'classnames';
+import cx from 'classnames'
 
-import PasswordInput from './PasswordInput';
-import styles from './Input.module.scss';
-import { CheckIcon } from '../../assets/svgs';
+import PasswordInput from './PasswordInput'
+import styles from './Input.module.scss'
+import { CheckIcon } from '../../assets/svgs'
 
-const Input = ({ setFuncEmail, setFuncPassword }) => {
-  const [inputEmail, setInputEmail] = useState('');
-  const [inputPassword, setInputPassword] = useState('');
-  const [ifEmailValid, setIfEmailValid] = useState(false);
-  const [isHidden, setIsHidden] = useState(true);
+function Input({ setFuncEmail, setFuncPassword }) {
+  const [inputEmail, setInputEmail] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
+  const [isEmailValid, setisEmailValid] = useState(false)
+  const [isHidden, setIsHidden] = useState(true)
 
-  const handleEmailChange = e => {
-    const regex = /[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-    setInputEmail(e.target.value);
-    if (e.target.value.match(regex)) setIfEmailValid(true);
-    else setIfEmailValid(false);
-  };
+  const handleEmailChange = (e) => {
+    const regex = /[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    setInputEmail(e.target.value)
+    if (e.target.value.match(regex)) {
+      setisEmailValid(true)
+      setIsHidden(true)
+    } else setisEmailValid(false)
+  }
 
-  const handleEmailBlur = e => {
-    e.preventDefault();
-    setIsHidden(!ifEmailValid && inputEmail.length > 0 ? true : false);
-  };
+  const handleEmailBlur = () => {
+    if (!isEmailValid && inputEmail.length > 0) setIsHidden(false)
+    else setIsHidden(true)
+  }
 
   useEffect(() => {
-    setFuncEmail(inputEmail);
-    setFuncPassword(inputPassword);
-  }, [inputEmail, inputPassword]);
+    setFuncEmail(inputEmail)
+    setFuncPassword(inputPassword)
+  }, [inputEmail, inputPassword, setFuncEmail, setFuncPassword])
 
   return (
     <div className={styles.inputDiv}>
       <div className={styles.inputContainer}>
-        <label htmlFor="email">E-mail</label>
+        <label htmlFor='email'>E-mail</label>
         <div>
           <input
-            type="email"
-            name="email"
-            placeholder="E-mail"
+            type='email'
+            name='email'
+            placeholder='E-mail'
             value={inputEmail}
             onChange={handleEmailChange}
             onBlur={handleEmailBlur}
           />
-          <label className={cx({ isHidden: isHidden })}>
+          <label htmlFor='email' className={cx(styles.labelInvalid, { [styles.isHidden]: isHidden })}>
             Invalid E-mail address.
           </label>
-          <CheckIcon />
+          <CheckIcon className={cx({ [styles.isEmailValid]: isEmailValid })} />
         </div>
       </div>
 
-      <PasswordInput
-        className="input-div passwd"
-        inputPassword={inputPassword}
-        setInputPassword={setInputPassword}
-      />
+      <PasswordInput className='input-div passwd' inputPassword={inputPassword} setInputPassword={setInputPassword} />
     </div>
-  );
-};
+  )
+}
 
-export default Input;
+export default Input
