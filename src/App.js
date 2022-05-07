796af87bb3896035959de983d4087331778407f2
chichoon <chichoon.choi@gmail.com>
2022-05-07 23:30:17 +0900

    Feat: Prettier 재적용

--- src/App.js
+++ src/App.js
@@ -1,62 +1,46 @@
-import { React, useState } from 'react';
+import { React, useState } from 'react'
 
-import Styled from 'styled-components';
-import { Dropdown, Input, Slider, Tab, Toggle } from './components';
+import Styled from 'styled-components'
+import { Dropdown, Input, Slider, Tab, Toggle } from './components'
 
-import './App.css';
+import './App.css'
 
 function App() {
-  const [toggleSelected, setToggleSelected] = useState('');
-  const [tabSelected, setTabSelected] = useState('');
-  const [sliderValue, setSliderValue] = useState(0);
-  const [inputEmail, setInputEmail] = useState('');
-  const [inputPassword, setInputPassword] = useState('');
-  const [dropdownSelected, setDropdownSelected] = useState('');
+  const [toggleSelected, setToggleSelected] = useState('')
+  const [tabSelected, setTabSelected] = useState('')
+  const [sliderValue, setSliderValue] = useState(0)
+  const [inputEmail, setInputEmail] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
+  const [dropdownSelected, setDropdownSelected] = useState('')
 
   return (
-    <div className="App">
+    <div className='App'>
       <ComponentWrapper>
-        <Toggle
-          firstString="기본"
-          secondString="상세"
-          setFunc={setToggleSelected}
-        />
-        <div className="component-value">Selected: {toggleSelected}</div>
+        <Toggle firstString='기본' secondString='상세' setFunc={setToggleSelected} />
+        <div className='component-value'>Selected: {toggleSelected}</div>
       </ComponentWrapper>
       <ComponentWrapper>
-        <Tab
-          selectorArr={['감자', '고구마', '카레라이스']}
-          setFunc={setTabSelected}
-        />
-        <div className="component-value">Selected: {tabSelected}</div>
+        <Tab selectorArr={['감자', '고구마', '카레라이스']} setFunc={setTabSelected} />
+        <div className='component-value'>Selected: {tabSelected}</div>
       </ComponentWrapper>
       <ComponentWrapper>
         <Slider setFunc={setSliderValue} />
-        <div className="component-value">Value: {sliderValue}</div>
+        <div className='component-value'>Value: {sliderValue}</div>
       </ComponentWrapper>
       <ComponentWrapper>
-        <Input
-          setFuncEmail={setInputEmail}
-          setFuncPassword={setInputPassword}
-        />
-        <div className="component-value">Email: {inputEmail}</div>
-        <div className="component-value">Password: {inputPassword}</div>
+        <Input setFuncEmail={setInputEmail} setFuncPassword={setInputPassword} />
+        <div className='component-value'>Email: {inputEmail}</div>
+        <div className='component-value'>Password: {inputPassword}</div>
       </ComponentWrapper>
       <ComponentWrapper>
         <Dropdown
-          dropdownArr={[
-            'BTCUSD.PERP',
-            'BCHUSD.PERP',
-            'ETHUSD.PERP',
-            'BANUSD.PERP',
-            'XPRUSD.PERP',
-          ]}
+          dropdownArr={['BTCUSD.PERP', 'BCHUSD.PERP', 'ETHUSD.PERP', 'BANUSD.PERP', 'XPRUSD.PERP']}
           setFunc={setDropdownSelected}
         />
-        <div className="component-value">Selected: {dropdownSelected} </div>
+        <div className='component-value'>Selected: {dropdownSelected} </div>
       </ComponentWrapper>
     </div>
-  );
+  )
 }
 
 const ComponentWrapper = Styled.div`
@@ -67,6 +51,6 @@ const ComponentWrapper = Styled.div`
     margin: 0.5rem;
     overflow: hidden;
   }
-`;
+`
 
-export default App;
+export default App
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import Input from './Input';
-import Slider from './Slider';
-import Tab from './Tab';
-import Toggle from './Toggle';
-import Dropdown from './Dropdown';
+import Input from './Input'
+import Slider from './Slider'
+import Tab from './Tab'
+import Toggle from './Toggle'
+import Dropdown from './Dropdown'
 
-export { Input, Slider, Tab, Toggle, Dropdown };
+export { Input, Slider, Tab, Toggle, Dropdown }
