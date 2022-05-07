2f12de192451efd46e90f35f509cc26fb5798a47
chichoon <chichoon.choi@gmail.com>
2022-05-07 23:27:53 +0900

    Feat: Tab 리팩토링 + scss로 옮긺

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,106 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-import Styled from 'styled-components';
+import cx from 'classnames'
 
-const Tab = ({ selectorArr, setFunc }) => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
+import styles from './Tab.module.scss'
 
-  const handleOnClick = (e, index) => {
-    e.preventDefault();
-    setSelectedIndex(index);
-  };
+function Tab({ selectorArr, setFunc }) {
+  const selectorLength = selectorArr.length
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const handleOnClick = (index) => {
+    setSelectedIndex(index)
+  }
 
   useEffect(() => {
-    setFunc([selectorArr[selectedIndex]]);
-  }, [selectedIndex]);
+    setFunc([selectorArr[selectedIndex]])
+  }, [setFunc, selectorArr, selectedIndex])
 
   return (
-    <TabWrapper length={selectorArr.length} index={selectedIndex}>
-      <div>
+    <div className={styles.tabDiv}>
+      <section className={styles.tabTop}>
         {selectorArr.map((v, i) => (
-          <SelectorDiv
-            className={selectedIndex === i ? 'selected' : ''}
-            key={i}
-            onClick={e => handleOnClick(e, i)}
-            length={selectorArr.length}
+          <button
+            type='button'
+            className={cx(styles.selectorDiv, { [styles.selectedDiv]: selectedIndex === i })}
+            key={`selector-arr-${v}`}
+            onClick={() => handleOnClick(i)}
+            style={{ width: `calc(24rem / ${selectorLength})` }}
           >
             {selectorArr[i]}
-          </SelectorDiv>
+          </button>
         ))}
-      </div>
-      <div>
-        <div></div>
-      </div>
-    </TabWrapper>
-  );
-};
+      </section>
+      <section className={styles.tabBottom}>
+        <div
+          className={styles.tabBar}
+          style={{
+            'margin-left': `calc(0.5rem + 24rem * ${selectedIndex / selectorLength})`,
+            width: `calc(24rem / ${selectorLength})`,
+          }}
+        />
+      </section>
+    </div>
+  )
+}
 
-const TabWrapper = Styled.div`
-  width: 30rem;
-  height: 3rem;
-
-  padding: 0.3rem;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  & > div {
-    width: 25rem;
-  }
-
-  & > div:nth-child(1){
-    height: 2.7rem;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-
-  & > div:nth-child(2) {
-    height: 0.3rem;
-
-    background-color: lightgray;
-
-    div {
-      width: calc(24rem / ${props => props.length});
-      height: 0.3rem;
-
-      margin-left: calc(0.5rem + 24rem * ${props =>
-        props.index / props.length});
-
-      background-color: teal;
-      transition: margin-left 0.2s;
-    }
-  }
-
-`;
-
-const SelectorDiv = Styled.div`
-  width: calc(24rem / ${props => props.length});
-
-  margin-bottom: 0.3rem;
-  background-color: white;
-  color: rgba(0, 0, 0, 0.3);
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  font-weight: 700;
-  transition: color 0.2s;
-
-  &.selected {
-    color: rgba(0, 0, 0, 1);
-  }
-
-  &:hover {
-    cursor: pointer;
-    &:not(.selected) {
-    color: rgba(0, 0, 0, 0.5);
-    }
-  }
-`;
-
-export default Tab;
+export default Tab
